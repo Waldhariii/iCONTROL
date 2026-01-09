d47fd289cfabbb0a3cd341ce7f41b7289fc7b465
Dany Gaudreault <danygaudreault@ton-domaine.com>
2026-01-09 14:26:08 -0500

    feat(core): add rules package (pure policy runner + result contract, micro-units)

---
+++ app/src/core/studio/rules/defaults.ts
@@ -0,0 +1,5 @@
+import type { Rule } from "./types";
+
+export function createDefaultRules(): Rule[] {
+  return [];
+}
---
+++ app/src/core/studio/rules/index.ts
@@ -0,0 +1,5 @@
+export * from "./result";
+export * from "./types";
+export * from "./guards";
+export * from "./runner";
+export * from "./defaults";
